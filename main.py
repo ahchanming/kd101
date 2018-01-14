3faf95b77afd5706cc2d77efc1131bee85114695
ahchanming@gmail.com <nemo11092>
2018-01-14 19:29:54 +0800

    批量从快递100爬取物流时效

--- main.py
+++ main.py
@@ -1,17 +1,56 @@
-#coding:utf-8
+# coding:utf-8
 
 
 import spider.kuaidi100
+import threading
 import json
+import time
 
-print (spider.kuaidi100.get_express_avg_days(1, 350213, 440515))
+fileInput = open("data/area.txt")
+area_codes = []
+start_codes = [121111, 350213, 320583]
+company_codes = [4]
 
-
-fileInput = open("area.txt")
 while True:
-    str = fileInput.readline()
-    if not str:
+    strLine = fileInput.readline()
+    if not strLine:
         break
-    code = fileInput.readline().split(',')[0]
-    print(code[0:6] + "," + spider.kuaidi100.get_express_avg_days(1, 320583, code[0:6]))
+    code = strLine.split(',')[0]
+    area_codes.append(strLine[0: 6])
+print("读取地区数据，数据总数为", len(area_codes))
 
+
+def read_avg_from_kd100(area_codes, start_code, express_company, resultmap):
+    print(str.format("准备从快递100爬取快递信息，开始地区为{}，快递公司编号为{}", start_code, express_company))
+    result = []
+    for end_code in area_codes:
+        days = spider.kuaidi100.get_express_avg_days(express_company, start_code, end_code)
+        if days != "None":
+            result.append({'start': start_code, 'end': end_code, 'days': days})
+    resultmap[(start_code, express_company)] = result
+
+
+def dump2file(resultmap):
+    while True:
+        time.sleep(3600)
+        file_output = open("output.txt", "w")
+        data = {'data': resultmap, 'version': time.time()}
+        file_output.write(json.dumps(data))
+        file_output.close()
+
+
+def dump_data_from_kd100():
+    threads = []
+    result_map = {}
+    for start in start_codes:
+        for company_code in company_codes:
+            print(str.format("创建从kd100读取线程，起始地址为{},快递公司为{}", start, company_code))
+            th = threading.Thread(target=read_avg_from_kd100, args=[area_codes, start, company_code, result_map])
+            threads.append(th)
+    for each in threads:
+        each.start()
+        each.join()
+    dump2file(result_map)
+
+
+dump_data_from_kd100()
--- spider/kuaidi100.py
+++ spider/kuaidi100.py
@@ -2,30 +2,30 @@ import urllib.request
 import json
 import re
 
+
 def get_express_detail(express_code, express_company):
     url = "https://www.kuaidi100.com/time/comtimecostinfo_5_CN350213_CN440515.htm?"
     dict = {}
     dict['type'] = express_company
     dict['postid'] = express_code
-    response = urllib.request.urlopen(url);
-    #response = json.load(r.read())
-    #print(response)
+    response = urllib.request.urlopen(url)
+    # response = json.load(r.read())
+    # print(response)
     print(response.read().decode("utf-8"))
 
 
 def get_express_avg_days(express_company, start_code, end_code):
-    url = "https://www.kuaidi100.com/time/comtimecostinfo_4_CN" + str (start_code) + "_CN" + str(end_code) + ".htm?"
+    url = "https://www.kuaidi100.com/time/comtimecostinfo_" + str(express_company) + "_CN" + str(
+        start_code) + "_CN" + str(end_code) + ".htm?"
     try:
-        response = urllib.request.urlopen(url);
+        response = urllib.request.urlopen(url, timeout=1)
         responseStr = response.read().decode("utf-8")
         index = responseStr.find("天 </td>")
-        subStr = responseStr[index - 20 : index]
-        #print(responseStr[index - 20 : index])
+        subStr = responseStr[index - 20: index]
         pattern = re.compile('[1-9]\d*.\d*|0.\d*[1-9]\d*')
         result = pattern.search(subStr)
         if (result == None):
             return "None"
-        return pattern.search(subStr).group(0)
+        return result.group(0)
     except:
         return "None"
-
